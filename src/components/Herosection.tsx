e67e23cbcb06c2a21b00d0d7b959099f5afb8e56
nimranaz148 <nimranaz148@gmail.com>
2024-11-10 20:44:19 +0500

    first

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -11,9 +11,9 @@ function Herosection() {
   return (
     <>
       <section className='bg-[#0a0908] w-full text-center pt-[7.5rem]'>
-        <p className='text-[2rem]'>Hey! I'm <span className='text-[#f6953a]'>Nimra Naz</span></p>
+        <p className='text-[2rem]'>Hey! I&apos;m <span className='text-[#f6953a]'>Nimra Naz</span></p>
 
-        <p className='text-[2rem]'><span className='text-[#f6953a]'>I</span>'m <span className='text-[#f6953a]'>A F</span>ront <span className='text-[#f6953a]'>E</span>nd <span className='text-[#f6953a]'>W</span>eb <span className='text-[#f6953a]'>D</span>eveloper</p>
+        <p className='text-[2rem]'><span className='text-[#f6953a]'>I</span>&apos;m <span className='text-[#f6953a]'>A F</span>ront <span className='text-[#f6953a]'>E</span>nd <span className='text-[#f6953a]'>W</span>eb <span className='text-[#f6953a]'>D</span>eveloper</p>
 
         <p className='mt-[1.25rem] mb-[2rem] text-[1.125rem] '>I can build user interface for web sites and applications with <span className='text-[#f6953a]'>React JS</span> and <span className='text-[#f6953a]'>Next.Js</span><br />I love the front end development.</p>
 
