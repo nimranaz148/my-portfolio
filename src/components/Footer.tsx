e1667e4674ba856a8a32f0d93f4fa046d27e5ac0
nimranaz148 <nimranaz148@gmail.com>
2024-11-10 21:23:59 +0500

    foot

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      <footer className="bg-[#0a0908] text-[#a9927d] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Taha Ahmed. All rights reserved.</footer>
+      <footer className="bg-[#0a0908] text-[#a9927d] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Nimra Naz. All rights reserved.</footer>
     </>
   )
 }
